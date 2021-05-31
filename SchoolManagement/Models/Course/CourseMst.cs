cf270a9b86bdba19bf3d4f10d47ead42285db4fc
Shahban Ali <shahbanalirajput@gmail.com>
2021-05-31 18:37:41 +0530

    Added a class file to the Model named CourseMst

---
+++ SchoolManagement/Models/Course/CourseMst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models.Course
+{
+    public class CourseMst
+    {
+        public string CourseName { get; set; }
+        public string Duration { get; set; }
+        public string CourseFees { get; set; }
+
+    }
+}
